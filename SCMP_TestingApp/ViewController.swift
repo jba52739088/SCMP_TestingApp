16a355a761fe9c3b110d018f051e1b5d2d4ed084
En-Yu, Huang <jba52739088@gmail.com>
2021-06-09 15:52:56 +0800

    Initial Commit

---
+++ SCMP_TestingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCMP_TestingApp
+//
+//  Created by 黃恩祐 on 2021/6/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
